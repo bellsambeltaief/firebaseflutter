2831b26f00cfc65eae1eaea0c861f28604fa92e3
Bellsam Beltaief <balsembeltaief90@gmail.com>
2024-05-02 20:58:56 +0100

    download image

--- lib/vendor_home/vendor_home.dart
+++ lib/vendor_home/vendor_home.dart
@@ -1,6 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:image_gallery_saver/image_gallery_saver.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:smart/common/button.dart';
 import 'package:smart/widgets/authentification/log_in/vendor_log_in_firebase.dart';
 import 'package:smart/widgets/firebase_auth_imp/firebase_auth_services.dart';
@@ -19,9 +23,71 @@ class VendorHome extends StatefulWidget {
 
 class _VendorHomeState extends State<VendorHome> {
   final FirebaseAuthService _auth = FirebaseAuthService();
-  @override
-  void initState() {
-    super.initState();
+
+  Future<String> _downloadImage(String imageUrl) async {
+    final dio = Dio();
+    final directory = await getTemporaryDirectory();
+    final savePath = '${directory.path}/cheque_image.jpg';
+
+    try {
+      await dio.download(imageUrl, savePath);
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('Image downloaded successfully'),
+        ),
+      );
+      return savePath;
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('Failed to download the image'),
+        ),
+      );
+      return '';
+    }
+  }
+
+  Future<bool> _saveImageToGallery(String imagePath) async {
+    final status = await Permission.storage.request();
+    if (status.isGranted) {
+      try {
+        final result = await ImageGallerySaver.saveFile(imagePath);
+        return result['isSuccess'];
+      } catch (e) {
+        if (kDebugMode) {
+          print('Failed to save image to gallery: $e');
+        }
+        return false;
+      }
+    } else {
+      if (kDebugMode) {
+        print('Permission denied');
+      }
+      return false;
+    }
+  }
+
+  void _showImageDialog(String imageUrl) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return Dialog(
+          child: GestureDetector(
+            onTap: () {
+              Navigator.of(context).pop();
+            },
+            child: SizedBox(
+              width: MediaQuery.of(context).size.width * 0.9,
+              height: MediaQuery.of(context).size.height * 0.9,
+              child: Image.network(
+                imageUrl,
+                fit: BoxFit.contain,
+              ),
+            ),
+          ),
+        );
+      },
+    );
   }
 
   @override
@@ -65,11 +131,35 @@ class _VendorHomeState extends State<VendorHome> {
                     itemBuilder: (BuildContext context, int index) {
                       String imageUrl = documents[index].get('downloadUrl');
                       return ListTile(
-                        leading: Image.network(
-                          imageUrl,
-                          width: 80,
-                          height: 80,
-                        ),
+                        leading: Stack(children: [
+                          Image.network(
+                            imageUrl,
+                            width: 80,
+                            height: 80,
+                          ),
+                          IconButton(
+                            icon: const Icon(Icons.file_download),
+                            onPressed: () async {
+                              String filePath = await _downloadImage(imageUrl);
+                              if (filePath.isNotEmpty) {
+                                bool success = await _saveImageToGallery(filePath);
+                                if (success) {
+                                  ScaffoldMessenger.of(context).showSnackBar(
+                                    const SnackBar(
+                                      content: Text('Image saved to gallery'),
+                                    ),
+                                  );
+                                } else {
+                                  ScaffoldMessenger.of(context).showSnackBar(
+                                    const SnackBar(
+                                      content: Text('Failed to save image to gallery'),
+                                    ),
+                                  );
+                                }
+                              }
+                            },
+                          ),
+                        ]),
                         title: Text(
                           'Cheque ${index + 1}',
                           style: TextStyle(
@@ -80,6 +170,9 @@ class _VendorHomeState extends State<VendorHome> {
                         subtitle: Text(
                           'Client: ${documents[index].get('userEmail')}',
                         ),
+                        onTap: () {
+                          _showImageDialog(imageUrl);
+                        },
                       );
                     },
                   ),
